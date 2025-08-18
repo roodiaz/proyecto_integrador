6e1b31a4011ead97c0519e77ca72d01b7272080e
roodiaz <roabigail.diaz@gmail.com>
2025-08-18 18:25:56 -0300

    add - ruteo en landing body

--- market_alerts/src/app/app.routes.ts
+++ market_alerts/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LandingBody } from './components/landing-body/landing-body';
+import { LoginForm } from './components/login-form/login-form';
+import { RegisterForm } from './components/register-form/register-form';
+import { AboutPage } from './components/about-page/about-page';
+import { ContactForm } from './components/contact-form/contact-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      component: LandingBody,
+      children: [
+        { path: '', redirectTo: 'login', pathMatch: 'full' },
+        { path: 'login', component: LoginForm },
+        { path: 'registro', component: RegisterForm },
+        { path: 'nosotros', component: AboutPage },
+        { path: 'contacto', component: ContactForm },
+      ],
+    },
+    { path: '**', redirectTo: '' }, // redirige al layout y este a /login
+  ];
