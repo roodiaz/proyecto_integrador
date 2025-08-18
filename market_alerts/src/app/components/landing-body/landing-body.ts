f08060038f39d968a12b557cc9948153d5c36cbd
roodiaz <roabigail.diaz@gmail.com>
2025-08-18 18:12:26 -0300

    add - ruteos en lading body

--- market_alerts/src/app/components/landing-body/landing-body.ts
+++ market_alerts/src/app/components/landing-body/landing-body.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-body',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule,
+    RouterLink,
+    RouterLinkActive,
     MatCardModule,
     MatButtonModule,
     MatDividerModule,
