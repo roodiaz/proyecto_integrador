3e9b07ff38757db25f9d94b6a337ee453de330bf
roodiaz <roabigail.diaz@gmail.com>
2025-08-19 20:28:41 -0300

    add - se implementa sidebar en dashboard

--- market_alerts/src/app/components/dashboard/dashboard.ts
+++ market_alerts/src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    Sidebar
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
 export class Dashboard {
+  showSidebar = true;
 
+  toggleSidebar() {
+    this.showSidebar = !this.showSidebar;
+  }
 }
