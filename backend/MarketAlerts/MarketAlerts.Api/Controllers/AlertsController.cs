1d3d308bdf853b48da1fea14a42044efbb4af1d1
rdiaz <rdiaz@pentass.com>
2026-04-06 11:19:51 -0300

    add - primera estructura backend + archivos docker

---
+++ backend/MarketAlerts/MarketAlerts.Api/Controllers/AlertsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketAlerts.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlertsController : ControllerBase
+    {
+    }
+}
